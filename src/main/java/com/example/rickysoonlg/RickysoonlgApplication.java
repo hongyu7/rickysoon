b2a1b80b1fd76c9de22678ecb078fa0e8096dc0f
Ricky <hongyu71@gmail.com>
2021-09-27 16:39:41 +0800

    submit sowething.

---
+++ src/main/java/com/example/rickysoonlg/RickysoonlgApplication.java
@@ -0,0 +1,13 @@
+package com.example.rickysoonlg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickysoonlgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickysoonlgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rickysoonlg/RickysoonlgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rickysoonlg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickysoonlgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
